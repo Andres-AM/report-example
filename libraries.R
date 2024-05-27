8a14a1178016c687f4852b2dfbe78451447d86fc
Andres-AM <andres.acunamarroquin@gmail.com>
2024-05-27 14:38:42 +0200

    report update

--- libraries.R
+++ libraries.R
@@ -1,17 +1,7 @@
 
 ## All packages 
 library(tidyverse)
-library(lubridate) 
-library(modelr) 
-# library(kableExtra)
-library(plotly)
-library(splines)
 library(knitr) 
-library(plotly)
-library(ggrepel)
-library(paletteer)
-library(flexdashboard)
-library(reactable)
 
 ## Other settings 
 theme_set(theme_bw())
